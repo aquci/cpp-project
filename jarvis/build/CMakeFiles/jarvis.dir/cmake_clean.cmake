eea4b4f785f389c36882e9a217f80c0bf564557d
aquci <archuserr@proton.me>
2026-05-27 02:40:21 +0300

    add cmake

---
+++ jarvis/build/CMakeFiles/jarvis.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jarvis.dir/link.d"
+  "CMakeFiles/jarvis.dir/jarvis.cpp.o"
+  "CMakeFiles/jarvis.dir/jarvis.cpp.o.d"
+  "CMakeFiles/jarvis.dir/jarvisUi.cpp.o"
+  "CMakeFiles/jarvis.dir/jarvisUi.cpp.o.d"
+  "jarvis"
+  "jarvis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jarvis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
